1fdf5056d8135652c75b33f29ece5d9e4894b1ff
Unknown <pierre.marez@isen.yncrea.fr>
2019-07-08 22:09:30 +0200

    add language and setup structure

--- src/js/language.js
+++ src/js/language.js
@@ -1,3 +1,15 @@
+/*Function which will set all the text in the selected language*/
 var putTxt = function putTxt (lang) {
-	console.log('test : lang = '+lang);	
+	let datas;
+	let jsonPath = "./language/"+lang+".json";//path to the json
+	console.log(jsonPath);
+	$.getJSON(jsonPath,function(json){
+		datas = json;
+	});
+	$('#corps').children().each(()=>{
+		if($(this).id == datas.$(this).id){
+			let id = $(this).id;
+			$(this).text(datas.id);
+		} 
+	});
 };
