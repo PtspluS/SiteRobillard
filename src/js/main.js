cd31f91d6d552c80417edf573614a989bbbcb470
Unknown <pierre.marez@isen.yncrea.fr>
2019-07-10 00:27:05 +0200

    change libs
    change math by plotly + add code and CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 (function () {
-	$('#selectLanguage').change(function(){
+	$('#selectLanguage').change(()=>{
 		let selectedLanguage = $(this).children("option:selected").val();
 		putTxt(selectedLanguage); 
 	});
+
+	(()=>$('#kRangeValue').text($("input[name=kRange]").val()))()
+
+	$('input[name=kRange]').on('input',()=>{
+		$('#kRangeValue').text($("input[name=kRange]").val());
+	})
 })();
