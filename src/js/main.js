e7213a6b54d399d897f5a7d2b153c2221427bbd0
Unknown <pierre.marez@isen.yncrea.fr>
2019-07-10 21:19:03 +0200

    finalisation of the structure

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,27 @@
 		putTxt(selectedLanguage); 
 	});
 
-	(()=>$('#kRangeValue').text($("input[name=kRange]").val()))()
+	(()=>$('#kRangeValue').text($("input[name=kRange]").val()))();
 
 	$('input[name=kRange]').on('input',()=>{
 		$('#kRangeValue').text($("input[name=kRange]").val());
-	})
+	});
+
+	(()=>$('#LRangeValue').text($("input[name=LRange]").val()))();
+
+	$('input[name=LRange]').on('input',()=>{
+		$('#LRangeValue').text($("input[name=LRange]").val());
+	});
+
+	$('#1d').click(function(){
+		alert("1D");
+	});
+
+	$('#2d').click(function(){
+		alert('2D');
+	});
+
+	$('#3d').click(function(){
+		alert('3D');
+	});
 })();
